e20bd09be362f6be5133fff3d3643ee8548cda3a
qinrenquan <qinrenquan@example.com>
2020-10-30 22:27:34 +0800

    change LED,delay

--- src/Blink.cpp
+++ src/Blink.cpp
@@ -9,7 +9,7 @@
 #include <Arduino.h>
 
 // Set LED_BUILTIN if it is not defined by Arduino framework
- #define LED_BUILTIN 10
+ #define LED_BUILTIN 9
 
 void setup()
 {
@@ -22,9 +22,9 @@ void loop()
   // turn the LED on (HIGH is the voltage level)
   digitalWrite(LED_BUILTIN, HIGH);
   // wait for a second
-  delay(1000);
+  delay(2000);
   // turn the LED off by making the voltage LOW
   digitalWrite(LED_BUILTIN, LOW);
    // wait for a second
-  delay(1000);
+  delay(2000);
 }
